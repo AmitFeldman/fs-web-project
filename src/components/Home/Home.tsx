476b3e224b82a05b62dd499bf35a964259b1ff34
Ofek Aziel <ofek200@gmail.com>
2020-03-02 14:58:23 +0200

    added title + blog post to the home screen

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,17 @@
 import React, {FC} from 'react';
+import Form from './Form';
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="row pt-5">
+        <div className="col-12 col-lg-6 offset-lg-3">
+          <h1 className="text-center">LightBlog</h1>
+        </div>
+        <Form />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
