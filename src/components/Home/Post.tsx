6b2ca98aa11ebd7579291e7176e5542bf52436f6
Ofek Aziel <ofek200@gmail.com>
2020-03-02 15:02:04 +0200

    fix html problem

--- src/components/Home/Post.tsx
+++ src/components/Home/Post.tsx
@@ -4,9 +4,7 @@ const Post: FC = () => {
   return (
     <div className="col-12 col-lg-6 offset-lg-3">
       <input className="form-control my-3" placeholder="Post Title" />
-      <textarea
-        className="form-control my-3"
-        placeholder="Post Author"></textarea>
+      <textarea className="form-control my-3" placeholder="Post Author" />
       <input className="form-control my-3" placeholder="Post Body" />
       <button className="btn btn-primary float-right">Submit</button>
     </div>
