af81e0f5b5ee1697e83c971f1e7289a162fa2219
Ofek Aziel <ofek200@gmail.com>
2020-03-02 14:59:28 +0200

    added form component to use in the home screen for showing posts

---
+++ src/components/Home/Form.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+
+const Form: FC = () => {
+  return (
+    <div className="col-12 col-lg-6 offset-lg-3">
+      <input className="form-control my-3" placeholder="Post Title" />
+      <textarea
+        className="form-control my-3"
+        placeholder="Post Author"></textarea>
+      <input className="form-control my-3" placeholder="Post Body" />
+      <button className="btn btn-primary float-right">Submit</button>
+    </div>
+  );
+};
+
+export default Form;
