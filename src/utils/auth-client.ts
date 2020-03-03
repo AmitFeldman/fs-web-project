e0b094b5f95a74ce1f65f6cf3fbc9248df71fddd
AmitF <amit123feldman@cs.colman.ac.il>
2020-03-03 14:44:56 +0200

    fixed refresh auth bug

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,9 +1,12 @@
 import {getMe, User} from './users-api';
 
 const LOCAL_STORAGE_KEY = '__auth_token__';
+const LOCAL_STORAGE_USER_KEY = '__local_user__';
 
-const setToken = ({_id}: User) => {
+const setToken = (user: User) => {
+  const {_id} = user;
   window.localStorage.setItem(LOCAL_STORAGE_KEY, _id);
+  window.localStorage.setItem(LOCAL_STORAGE_USER_KEY, JSON.stringify(user));
 };
 
 const getToken = (): string | null => {
@@ -13,6 +16,17 @@ const getToken = (): string | null => {
 const removeToken = (): Promise<void> => {
   window.localStorage.removeItem(LOCAL_STORAGE_KEY);
   return Promise.resolve();
+};
+
+// When the page is refreshed we send a request to get the logged on user by
+// their token. We can use the local user that is also saved when setToken is
+// called so that we don't have to redirect before the request is resolved
+const getLocalUser = (): User | null => {
+  const localUser = window.localStorage.getItem(LOCAL_STORAGE_USER_KEY);
+
+  if (localUser) return JSON.parse(localUser);
+
+  return null;
 };
 
 function getUser(): Promise<User | null> {
@@ -29,4 +43,4 @@ function getUser(): Promise<User | null> {
   });
 }
 
-export {setToken, getToken, removeToken, getUser};
+export {setToken, getToken, removeToken, getUser, getLocalUser};
