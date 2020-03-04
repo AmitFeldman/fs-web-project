08a84eec5d1f4533984063d4ea17dfd0016fdce5
AmitF <amit123feldman@cs.colman.ac.il>
2020-03-04 16:32:09 +0200

    add secret to gitignore

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
+import secrets from './secrets';
+
 const express = {
   serverPort: 8080,
   socketPort: 80,
@@ -9,7 +11,7 @@ const mongo = {
   host: 'alon.documents.azure.com',
   port: '10255',
   database: 'fs-project',
-  password: '4GCiae7Q0sKtC14CAssmliC4IOxiIp0RX4PfQvXh7ZD6sWArMUOZTcawUpAA9jRshn3ys8DrfihaFXqPSLkfEQ==',
+  password: secrets.mongoPassword,
 };
 
 const {user, password, host, port, database} = mongo;
