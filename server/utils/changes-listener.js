dbefd37c780b4fb480612f925c3085d1c1db8c68
AmitF <amit123feldman@cs.colman.ac.il>
2020-03-04 21:16:20 +0200

    added todo

--- server/utils/changes-listener.js
+++ server/utils/changes-listener.js
@@ -2,11 +2,11 @@ import Post from '../models/Post';
 import {emitEvent} from './socket-service';
 
 const NEW_POST_EVENT = 'NEW_POST';
-// const modelWatcher = (model, event, callback) => {
-//   Post.watch().on('insert', data => {
-//     callback(data);
-//   });
-// };
+
+// Todo: change to mongo watch
+// Post.watch().on('insert', data => {
+//   callback(data);
+// });
 
 const onNewPost = (post) => {
   emitEvent('NEW_POST', post);
