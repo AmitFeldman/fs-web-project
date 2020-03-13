225d6b8d669ff7d7b3306970555301cb3cba01fe
AmitF <amit123feldman@cs.colman.ac.il>
2020-03-13 21:10:32 +0200

    refactor alert to context

--- src/components/AlertSnackbar/AlertSnackbar.tsx
+++ src/components/AlertSnackbar/AlertSnackbar.tsx
@@ -1,25 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import Button from '@material-ui/core/Button';
+import {Link as RouterLink} from 'react-router-dom';
+import {AlertData} from '../../context/AlertContext';
 
 interface AlertSnackbarProps {
-  message: string;
-  button?: JSX.Element;
-  setMessage: (newState: string) => void;
+  alertData: AlertData;
 }
 
-const AlertSnackbar: FC<AlertSnackbarProps> = ({
-  message,
-  button,
-  setMessage,
-}) => {
-  const handleClose = (event: any, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
+const AlertSnackbar: FC<AlertSnackbarProps> = ({alertData}) => {
+  const [open, setOpen] = useState<boolean>(false);
 
-    setMessage('');
+  useEffect(() => {
+    setOpen(true);
+  }, [alertData]);
+
+  const handleClose = (event: any, reason?: string) => {
+    if (reason !== 'clickaway') {
+      setOpen(false);
+    }
   };
 
   return (
@@ -28,17 +29,26 @@ const AlertSnackbar: FC<AlertSnackbarProps> = ({
         vertical: 'bottom',
         horizontal: 'left',
       }}
-      open={message !== ''}
+      open={open}
       autoHideDuration={4000}
       onClose={handleClose}
-      message={message}
+      message={alertData.message}
       action={
-        <React.Fragment>
-          {button}
+        <>
+          {alertData.redirectPath && (
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              component={RouterLink}
+              to={alertData.redirectPath}>
+              {alertData.buttonText || ''}
+            </Button>
+          )}
           <IconButton size="small" color="inherit" onClick={handleClose}>
             <CloseIcon fontSize="small" />
           </IconButton>
-        </React.Fragment>
+        </>
       }
     />
   );
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,9 +4,8 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
-import {Link as RouterLink} from 'react-router-dom';
 import {UserErrorCode} from '../../utils/users-api';
-import AlertSnackbar from '../../components/AlertSnackbar/AlertSnackbar';
+import {useAlert} from '../../context/AlertContext';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,70 +19,61 @@ const useStyles = makeStyles((theme: Theme) =>
 const Login: FC = () => {
   const {login} = useAuth();
   const {root} = useStyles();
+  const {alert} = useAlert();
 
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [error, setError] = useState<string>('');
 
   const loginUser = () => {
     login({username, password}).catch(err => {
-      if (err.code === UserErrorCode.USER_NOT_FOUND) setError(err.msg);
+      if (err.code === UserErrorCode.USER_NOT_FOUND) {
+        alert({
+          message: err.msg,
+          redirectPath: '/register',
+          buttonText: 'Create a User',
+        });
+      }
     });
   };
 
   return (
-    <>
-      <ValidatorForm onSubmit={loginUser}>
-        <Grid container className={root} direction="column" spacing={3}>
-          <h1>Login</h1>
-          <Grid item>
-            <TextValidator
-              name="Username"
-              value={username}
-              onChange={(e: React.FormEvent<HTMLInputElement>) => {
-                setUsername(e.currentTarget.value);
-              }}
-              label="Username"
-              variant="outlined"
-              validators={['required']}
-              errorMessages={['Username is required']}
-            />
-          </Grid>
-          <Grid item>
-            <TextValidator
-              name="Password"
-              type="password"
-              value={password}
-              onChange={(e: React.FormEvent<HTMLInputElement>) => {
-                setPassword(e.currentTarget.value);
-              }}
-              label="Password"
-              variant="outlined"
-              validators={['required']}
-              errorMessages={['Password is required']}
-            />
-          </Grid>
-          <Grid item>
-            <Button type="submit" variant="contained" color="primary">
-              Submit
-            </Button>
-          </Grid>
+    <ValidatorForm onSubmit={loginUser}>
+      <Grid container className={root} direction="column" spacing={3}>
+        <h1>Login</h1>
+        <Grid item>
+          <TextValidator
+            name="Username"
+            value={username}
+            onChange={(e: React.FormEvent<HTMLInputElement>) => {
+              setUsername(e.currentTarget.value);
+            }}
+            label="Username"
+            variant="outlined"
+            validators={['required']}
+            errorMessages={['Username is required']}
+          />
         </Grid>
-      </ValidatorForm>
-      <AlertSnackbar
-        message={error}
-        button={
-          <Button
-            color="primary"
-            size="small"
-            component={RouterLink}
-            to="/register">
-            Register
+        <Grid item>
+          <TextValidator
+            name="Password"
+            type="password"
+            value={password}
+            onChange={(e: React.FormEvent<HTMLInputElement>) => {
+              setPassword(e.currentTarget.value);
+            }}
+            label="Password"
+            variant="outlined"
+            validators={['required']}
+            errorMessages={['Password is required']}
+          />
+        </Grid>
+        <Grid item>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
           </Button>
-        }
-        setMessage={setError}
-      />
-    </>
+        </Grid>
+      </Grid>
+    </ValidatorForm>
   );
 };
 
